72c85251c1141f574b8daa5e6a491afdc6a27024
Maria Fernanda Flores <mafer_271099@live.com>
2026-07-03 12:25:04 -0500

    Nuevo DataBase, implementado dialogos de pedido de los clientesy actualizacion de escena junto con el timer.

--- Assets/_ProjectFolder/Code/Scripts/Emergency/RequestCustomer.cs
+++ Assets/_ProjectFolder/Code/Scripts/Emergency/RequestCustomer.cs
@@ -1,11 +1,14 @@
 using System.Collections.Generic;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 public class RequestCustomer : MonoBehaviour
 {
     public Request[] ingredientsRequest;
     public GameObject recipeContainer;
+    public DialogManager dialogManager;
 
     private RoundManager _roundManager;
     private Card[] _cards;
@@ -14,6 +17,9 @@ public class RequestCustomer : MonoBehaviour
     private List<int> firstIngredients = new List<int> { 1, 6, 7 };
     private int currentValueRequest;
     private bool match;
+
+    public UnityEvent onMatch;
+    public UnityEvent onMatchLose;
 
     private void OnEnable() => _roundManager.onRoundBegin += SetRequest;
     private void OnDisable() => _roundManager.onRoundBegin -= SetRequest;
@@ -29,44 +35,61 @@ public class RequestCustomer : MonoBehaviour
         {
             int randomIndex = Random.Range(0, firstIngredients.Count);
             currentValueRequest = randomIndex;
+            dialogManager.PlayAtIndex(firstIngredients[currentValueRequest]);
             Debug.Log(ingredientsRequest[firstIngredients[currentValueRequest]].textRequest);
-            firstRequest = false;
             return;
         }
 
         int valueRandom = Random.Range(0, ingredientsRequest.Length - 1);
         currentValueRequest = valueRandom;
+        dialogManager.PlayAtIndex(currentValueRequest);
         Debug.Log(ingredientsRequest[currentValueRequest].textRequest);
     }
 
     public void CheckRequest()
     {
         _cards = recipeContainer.GetComponentsInChildren<Card>();
-        if (_cards == null) return;
+        if (_cards.Length == 0) return;
         foreach (var card in _cards)
         {
-            if(card.Ingredient == ingredientsRequest[currentValueRequest].requestIngredient)
+            if (firstRequest)
             {
-                match = true;
+                if (card.Ingredient == ingredientsRequest[firstIngredients[currentValueRequest]].requestIngredient)
+                {
+                    match = true;
+                }
+            }
+            else
+            {
+                if (card.Ingredient == ingredientsRequest[currentValueRequest].requestIngredient)
+                {
+                    match = true;
+                }
             }
         }
 
         if (match)
         {
             Debug.Log("Match");
+            onMatch?.Invoke();
+            dialogManager.Stop();
         }
         else
         {
             Debug.Log("No points");
+            onMatchLose?.Invoke();
+            dialogManager.Stop();
         }
 
         match = false;
+        firstRequest = false;
     }
 }
 
 [System.Serializable]
 public class Request
 {
+    //El testRequest es solo de testeo, ya se integro junto con el DialogManager
     public string textRequest;
     public SO_IngredientBase requestIngredient;
 }
