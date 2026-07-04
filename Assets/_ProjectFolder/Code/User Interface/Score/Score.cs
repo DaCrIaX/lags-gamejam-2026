d77c8ca50b4b9f6c832d6924adca4b9cd225416b
MauricioPisconte <66890909+MauricioPisconte@users.noreply.github.com>
2026-07-04 00:48:29 -0500

    gameplay subido

--- Assets/_ProjectFolder/Code/User Interface/Score/Score.cs
+++ Assets/_ProjectFolder/Code/User Interface/Score/Score.cs
@@ -1,32 +1,93 @@
 using System;
+using System.Collections;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Pool;
 
-public class Score : PoolSingleBehaviour<ScoreParticle>
+public class Score : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI _text;
+    [SerializeField, Min(0f)] private float _deltaDisplayDuration = 0.75f;
 
     private int _score;
+    private Coroutine _showTotalCoroutine;
+
     public int CurrentScore => _score;
     public event Action<int> onScoreChanged;
 
+    private void Awake()
+    {
+        CacheTextReference();
+        RefreshText();
+    }
+
+    private void OnValidate()
+    {
+        CacheTextReference();
+        RefreshText();
+    }
+
     public void AddScore(int value)
     {
-        var particle = _pool.Get() as ScoreParticle;
-        particle.SetText(value);
-
         _score += value;
-        RefreshText();
+        ShowDelta(value);
         onScoreChanged?.Invoke(_score);
     }
 
     public void ResetScore()
     {
         _score = 0;
+        StopShowTotalCoroutine();
         RefreshText();
         onScoreChanged?.Invoke(_score);
     }
 
-    private void RefreshText() => _text.SetText($"score: {_score}");
+    private void RefreshText()
+    {
+        CacheTextReference();
+
+        if (_text != null)
+        {
+            _text.SetText(_score.ToString());
+        }
+    }
+
+    private void ShowDelta(int value)
+    {
+        CacheTextReference();
+        StopShowTotalCoroutine();
+
+        if (_text != null)
+        {
+            string sign = value >= 0 ? "+" : string.Empty;
+            _text.SetText($"{sign}{value}");
+        }
+
+        _showTotalCoroutine = StartCoroutine(ShowTotalAfterDelay());
+    }
+
+    private IEnumerator ShowTotalAfterDelay()
+    {
+        yield return new WaitForSeconds(_deltaDisplayDuration);
+        _showTotalCoroutine = null;
+        RefreshText();
+    }
+
+    private void CacheTextReference()
+    {
+        if (_text == null)
+        {
+            TryGetComponent(out _text);
+        }
+    }
+
+    private void StopShowTotalCoroutine()
+    {
+        if (_showTotalCoroutine == null)
+        {
+            return;
+        }
+
+        StopCoroutine(_showTotalCoroutine);
+        _showTotalCoroutine = null;
+    }
 }
--- Assets/_ProjectFolder/Code/User Interface/Timer/TimerUIBar.cs
+++ Assets/_ProjectFolder/Code/User Interface/Timer/TimerUIBar.cs
@@ -1,11 +1,22 @@
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 public class TimerUIBar : TimerBase
 {
     [SerializeField] private Image _image;
     [SerializeField] private Gradient _gradient;
+
+    [Header("Z Rotation")]
+    [FormerlySerializedAs("_zMovementTarget")]
+    [SerializeField] private Transform _zRotationTarget;
+    [FormerlySerializedAs("_zAtFull")]
+    [SerializeField] private float _zRotationAtFull = 0f;
+    [FormerlySerializedAs("_zAtEmpty")]
+    [SerializeField] private float _zRotationAtEmpty = 360f;
+    [FormerlySerializedAs("_useLocalPosition")]
+    [SerializeField] private bool _useLocalRotation = true;
 
     [SerializeField] private UnityEvent _onTimeout;
 
@@ -13,9 +24,33 @@ public class TimerUIBar : TimerBase
     {
         _image.fillAmount = value;
         _image.color = _gradient.Evaluate(value);
+        UpdateZRotation(value);
     }
     protected override void OnCompleteTimer()
     {
+        OnTimerUpdate(0f);
         _onTimeout.Invoke();
+    }
+
+    private void UpdateZRotation(float fillAmount)
+    {
+        if (_zRotationTarget == null)
+        {
+            return;
+        }
+
+        float zRotation = Mathf.Lerp(_zRotationAtEmpty, _zRotationAtFull, fillAmount);
+
+        if (_useLocalRotation)
+        {
+            Vector3 localEulerAngles = _zRotationTarget.localEulerAngles;
+            localEulerAngles.z = zRotation;
+            _zRotationTarget.localEulerAngles = localEulerAngles;
+            return;
+        }
+
+        Vector3 eulerAngles = _zRotationTarget.eulerAngles;
+        eulerAngles.z = zRotation;
+        _zRotationTarget.eulerAngles = eulerAngles;
     }
 }
